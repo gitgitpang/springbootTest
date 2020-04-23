679f48ea57ed054ebeb90414ded6008a3e66a97e
gitgitpang <434392284@qq.com>
2020-04-23 17:51:53 +0800

    C.class add pang merge master branch

--- src/test/java/com/test/C.java
+++ src/test/java/com/test/C.java
@@ -2,6 +2,6 @@ package com.test;
 
 public class C {
     public static void main(String[] args) {
-        System.out.println("提交到master分支");
+        System.out.println("提交到master分支 pang分支和master分支合并");
     }
 }
