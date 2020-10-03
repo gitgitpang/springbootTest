ee4ef414ef2f53e7236f64211a10a641fabea75c
gitgitpang <434392284@qq.com>
2020-10-03 17:34:33 +0800

    添加emp和dept类以及对应的mapper.xml

---
+++ src/main/java/com/springboot/mapper/DeptMapper.java
@@ -0,0 +1,17 @@
+package com.springboot.mapper;
+
+import com.springboot.pojo.Dept;
+
+public interface DeptMapper {
+    int deleteByPrimaryKey(Integer deptno);
+
+    int insert(Dept record);
+
+    int insertSelective(Dept record);
+
+    Dept selectByPrimaryKey(Integer deptno);
+
+    int updateByPrimaryKeySelective(Dept record);
+
+    int updateByPrimaryKey(Dept record);
+}
---
+++ src/main/java/com/springboot/mapper/EmpMapper.java
@@ -0,0 +1,17 @@
+package com.springboot.mapper;
+
+import com.springboot.pojo.Emp;
+
+public interface EmpMapper {
+    int deleteByPrimaryKey(Integer empno);
+
+    int insert(Emp record);
+
+    int insertSelective(Emp record);
+
+    Emp selectByPrimaryKey(Integer empno);
+
+    int updateByPrimaryKeySelective(Emp record);
+
+    int updateByPrimaryKey(Emp record);
+}
